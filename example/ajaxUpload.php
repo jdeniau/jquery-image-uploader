3f5179ae0eeba76ec5257852914f928b3980a9cc
Julien Deniau <julien.deniau@gmail.com>
2013-10-18 16:18:15 +0200

    update example

--- example/ajaxUpload.php
+++ example/ajaxUpload.php
@@ -20,48 +20,73 @@ class File_Streamer
 
     public function receive()
     {
-        $file = $_FILES['file'];
+        $isFile = false;
 
-         if (is_uploaded_file($file['tmp_name'])) {
-                $tmpName = basename($file['name']);
+        if (isset($_FILES['file'])) {
+            $file = $_FILES['file'];
+
+            if (is_uploaded_file($file['tmp_name'])) {
+                $filename = $file['name'];
+            }
+
+            $isFile = true;
+        }
+
+        if (isset($_POST['url'])) {
+            $filename = $_POST['url'];
+        }
+
+        if (!empty($filename)) {
+            try {
+                $tmpName = basename($filename);
                 $fileName = date('Ymd-His-') . mt_rand() . '-' . $tmpName;
                 $uploadFile = $this->path . $fileName;
 
-                if (!move_uploaded_file($file['tmp_name'], $uploadFile)) {
+                if ($isFile) {
+                    if (!move_uploaded_file($file['tmp_name'], $uploadFile)) {
                         throw new UploadException('Unable to upload file');
+                    }
+                } else {
+                    $content = @file_get_contents($filename);
+                    if ($content) {
+                        file_put_contents($uploadFile, $content);
+                    }
                 }
 
                 if (!file_exists($uploadFile)) {
-                        throw new UploadException('Unable to upload file : file not found');
+                    throw new UploadException('Unable to upload file : file not found');
                 }
 
                 if (filesize($uploadFile) > 5242880) {
-                        throw new UploadException('Unable to upload file : file too big');
+                    throw new UploadException('Unable to upload file : file too big');
                 }
 
                 $fileInfo = @getimagesize($uploadFile);
 
                 if (empty($fileInfo)) {
-                        throw new UploadException('File is not an image');
+                    throw new UploadException('File is not an image');
                 }
 
                 $allowedMimeType = array(
                         'image/gif',
                         'image/jpeg',
                         'image/png'
-                );
+                        );
 
                 if (!in_array($fileInfo['mime'], $allowedMimeType)) {
-                        throw new UploadException('File type not allowed');
+                    throw new UploadException('File type not allowed');
                 }
 
                 /*
-                if ($fileInfo[0] > 3000 || $fileInfo[1] > 3000) {
-                        throw new UploadException('Unable to upload file : file too big');
-                }
-                */
+                   if ($fileInfo[0] > 3000 || $fileInfo[1] > 3000) {
+                   throw new UploadException('Unable to upload file : file too big');
+                   }
+                 */
 
                 return $fileName;
+            } catch (UploadException $e) {
+                @unlink($uploadFile);
+            }
         }
 
 
@@ -70,15 +95,19 @@ class File_Streamer
             throw new Exception('No file uploaded!');
         }
 
-        file_put_contents(
-            $this->path . $this->fileName,
-            file_get_contents("php://input")
-        );
+        //file_put_contents(
+        //    $this->path . $this->fileName,
+        //    file_get_contents("php://input")
+        //);
 
         return true;
     }
 }
 
-$fs = new File_Streamer();
-$fs->setDestination('/tmp/testjdu/');
-echo $fs->receive();
+try {
+    $fs = new File_Streamer();
+    $fs->setDestination('/tmp/testjdu/');
+    echo $fs->receive();
+} catch (\Exception $e) {
+    header('HTTP/1.1 500 Internal Server Error');
+}
